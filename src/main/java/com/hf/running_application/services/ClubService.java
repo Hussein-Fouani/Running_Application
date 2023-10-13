9805bdad5a0f50eec633652bbf3ce487ed095d56
Hussein <social.101.acc@gmail.com>
2023-10-13 20:19:53 +0300

    added the method to save club in the db

--- src/main/java/com/hf/running_application/services/ClubService.java
+++ src/main/java/com/hf/running_application/services/ClubService.java
@@ -1,9 +1,12 @@
 package com.hf.running_application.services;
 
+import com.hf.running_application.entities.Club;
 import com.hf.running_application.model.clubDTO;
 
 import java.util.List;
 
 public interface ClubService {
     List<clubDTO> findAllClubs();
+
+    Club save(Club club);
 }

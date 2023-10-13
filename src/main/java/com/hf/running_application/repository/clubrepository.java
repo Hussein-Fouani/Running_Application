435175661bcf5ae20128e4ccb89752d4485017b9
Hussein <social.101.acc@gmail.com>
2023-10-13 13:52:19 +0300

    repository responsible for data club creation and operation with DB

---
+++ src/main/java/com/hf/running_application/repository/clubrepository.java
@@ -0,0 +1,8 @@
+package com.hf.running_application.repository;
+
+import com.hf.running_application.model.Club;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface clubrepository extends JpaRepository<Club,Long> {
+
+}

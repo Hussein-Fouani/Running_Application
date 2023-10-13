e8505ba5c4ae4d31ed449de2dcd431a7017d425d
Hussein <social.101.acc@gmail.com>
2023-10-13 15:55:43 +0300

    fixed error in modelmapper

--- src/main/java/com/hf/running_application/RunningApplication.java
+++ src/main/java/com/hf/running_application/RunningApplication.java
@@ -1,10 +1,21 @@
 package com.hf.running_application;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
 public class RunningApplication {
+
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
 
     public static void main(String[] args) {
         SpringApplication.run(RunningApplication.class, args);

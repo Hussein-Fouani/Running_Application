d5739e1a782bdfba47586541e860f8104d56522c
Hussein <social.101.acc@gmail.com>
2023-10-13 20:17:50 +0300

    created two mapping for creating a new Club through form submission

--- src/main/java/com/hf/running_application/controllers/ClubController.java
+++ src/main/java/com/hf/running_application/controllers/ClubController.java
@@ -1,5 +1,6 @@
 package com.hf.running_application.controllers;
 
+import com.hf.running_application.entities.Club;
 import com.hf.running_application.model.clubDTO;
 import com.hf.running_application.services.ClubService;
 import lombok.RequiredArgsConstructor;
@@ -7,11 +8,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/clubs")
 public class ClubController {
 
     private ClubService clubService;
@@ -22,11 +27,23 @@ public class ClubController {
     }
 
 
-    @GetMapping("/clubs")
+    @GetMapping
     public String getAllClubs(Model model){
         List<clubDTO> clubs= clubService.findAllClubs();
         model.addAttribute("clubs",clubs);
         return "clubs-list";
     }
 
+    @GetMapping("new")
+    public String createClubForm(Model model){
+        Club club = new Club();
+        model.addAttribute("club",club);
+        return "clubs-create";
+    }
+    @PostMapping("/new")
+    public String createClub(@ModelAttribute("club") Club club){
+        clubService.save(club);
+        return "redirect:/clubs";
+
+    }
 }

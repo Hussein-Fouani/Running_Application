05e720c1bb9358fa53dad02920e5a36afe0d555d
Hussein <social.101.acc@gmail.com>
2023-10-14 12:27:47 +0300

    added mapping for the endpoints for editing requests

--- src/main/java/com/hf/running_application/controllers/ClubController.java
+++ src/main/java/com/hf/running_application/controllers/ClubController.java
@@ -7,10 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -34,7 +32,7 @@ public class ClubController {
         return "clubs-list";
     }
 
-    @GetMapping("new")
+    @GetMapping("/new")
     public String createClubForm(Model model){
         Club club = new Club();
         model.addAttribute("club",club);
@@ -45,5 +43,18 @@ public class ClubController {
         clubService.save(club);
         return "redirect:/clubs";
 
+    }
+    @GetMapping("/clubs/{clubId}/edit")
+    public String editClubForm(@PathVariable("clubId") long clubId,Model model){
+        clubDTO club = clubService.findClubById(clubId);
+        model.addAttribute("club",club);
+        return "clubs-edit";
+    }
+
+    @PostMapping("/{clubId}/edit")
+    public String updateClub(@PathVariable Long clubId, @ModelAttribute("club") clubDTO clubdto, BindingResult result){
+        clubdto.setId(clubId);
+        clubService.updateClub(clubdto);
+        return "redirect:/clubs";
     }
 }

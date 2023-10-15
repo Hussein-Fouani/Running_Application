d659a0bd4b840686dd9167bb0dc81cfbb7fa1302
Hussein <social.101.acc@gmail.com>
2023-10-15 17:21:55 +0300

    added a query by using JPQL for searching clubs by title

--- src/main/java/com/hf/running_application/repository/clubrepository.java
+++ src/main/java/com/hf/running_application/repository/clubrepository.java
@@ -2,7 +2,15 @@ package com.hf.running_application.repository;
 
 import com.hf.running_application.entities.Club;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface clubrepository extends JpaRepository<Club,Long> {
+
+    Optional<Club> findByTitle(String string);
+    @Query("select c from Club c where c.title like concat('%',:query,'%') ")
+    List<Club> searchClubs(String query);
 
 }

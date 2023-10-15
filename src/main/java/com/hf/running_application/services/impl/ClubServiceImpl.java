a78e55a02d2a507d88799e7594ce4f7409ca91a6
Hussein <social.101.acc@gmail.com>
2023-10-15 17:18:46 +0300

    refactored the mapping methods along the class to apply singleton concept

--- src/main/java/com/hf/running_application/services/impl/ClubServiceImpl.java
+++ src/main/java/com/hf/running_application/services/impl/ClubServiceImpl.java
@@ -1,54 +1,61 @@
 package com.hf.running_application.services.impl;
 
 import com.hf.running_application.exceptions.ClubNotFoundException;
-import com.hf.running_application.model.clubDTO;
+import com.hf.running_application.model.ClubDTO;
 import com.hf.running_application.entities.Club;
 import com.hf.running_application.repository.clubrepository;
 import com.hf.running_application.services.ClubService;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
-
+import com.hf.running_application.mappers.ClubMapper;
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static com.hf.running_application.mappers.ClubMapper.mapToClub;
+import static com.hf.running_application.mappers.ClubMapper.mapToClubDTO;
 
 @Service
 public class ClubServiceImpl implements ClubService {
     private clubrepository repository ;
-    private ModelMapper modelMapper ;
 
     public ClubServiceImpl(clubrepository repository, ModelMapper modelMapper) {
         this.repository = repository;
-        this.modelMapper = modelMapper;
     }
 
     @Override
-    public List<clubDTO> findAllClubs() {
+    public List<ClubDTO> findAllClubs() {
         List<Club> getallclubs = repository.findAll();
-        return getallclubs.stream().map(this::mapToClubDTO).collect(Collectors.toList());
+        return getallclubs.stream().map(ClubMapper::mapToClubDTO).collect(Collectors.toList());
     }
 
     @Override
-    public Club save(Club club) {
-        return repository.save(club);
-
+    public Club save(ClubDTO club) {
+        Club club1 =mapToClub(club);
+        repository.save(club1);
+        return club1;
     }
 
     @Override
-    public  clubDTO  findClubById(long clubId) {
+    public ClubDTO findClubById(long clubId) {
         Club clubs  = repository.findById(clubId).orElseThrow(()-> new ClubNotFoundException("Club Not Found",clubId));
         return mapToClubDTO(clubs);
     }
 
     @Override
-    public void updateClub(clubDTO clubdto) {
+    public void updateClub(ClubDTO clubdto) {
         Club club= mapToClub(clubdto);
         repository.save(club);
     }
 
-    private Club mapToClub(clubDTO DTO){
-        return modelMapper.map(DTO,Club.class);
+    @Override
+    public void deletebyId(Long clubId) {
+        Club club = repository.findById(clubId).orElseThrow(()-> new ClubNotFoundException("Club Not Found",clubId));
+        repository.deleteById(clubId);
     }
-    private clubDTO mapToClubDTO(Club club){
-        return modelMapper.map(club,clubDTO.class);
+
+    @Override
+    public List<ClubDTO> searchClubs(String title) {
+        return  repository.searchClubs(title).stream().map(ClubMapper::mapToClubDTO).collect(Collectors.toList());
     }
+
 }

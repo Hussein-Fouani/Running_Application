00308e1a9fef627930cb3213fc6036b1dd23b8de
Hussein <social.101.acc@gmail.com>
2023-10-15 17:30:28 +0300

    refactored class with adding validation

--- src/main/java/com/hf/running_application/model/clubDTO.java
+++ src/main/java/com/hf/running_application/model/clubDTO.java
@@ -1,5 +1,6 @@
 package com.hf.running_application.model;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,10 +12,13 @@ import java.time.LocalDateTime;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class clubDTO {
+public class ClubDTO {
     private Long Id;
+    @NotEmpty(message = "Club Title shouldn't be Empty")
     private String title;
+    @NotEmpty(message = "PhotoUrl Title shouldn't be Empty")
     private String photoUrl;
+    @NotEmpty(message = "Content Title shouldn't be Empty")
     private String content;
     private LocalDateTime createdOn;
     private LocalDateTime updatedOn;
